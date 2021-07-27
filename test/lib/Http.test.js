79c2ac2aa832b441642920223fbeeb98f0ba2c49
Pawel Psztyc <jarrodek@gmail.com>
2021-07-27 00:51:54 -0700

    test: adding more tests
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- test/lib/Http.test.js
+++ test/lib/Http.test.js
@@ -1,4 +1,7 @@
+
+// eslint-disable-next-line import/no-unresolved
 import { assert } from '@esm-bundle/chai';
+import sinon from 'sinon';
 import { Http } from '../../index.js';
 
 describe('Http', () => {
@@ -250,6 +253,160 @@ describe('Http.headers', () => {
     it('has the rel', () => {
       const result = http.headers.link();
       assert.include(result, 'rel="');
+    });
+  });
+
+  describe('contentType()', () => {
+    /** @type Http */
+    let http;
+  
+    beforeEach(() => {
+      http = new Http();
+    });
+
+    it('returns a string', () => {
+      const result = http.headers.contentType();
+      assert.typeOf(result, 'string');
+    });
+  });
+});
+
+describe('Http.payload', () => {
+  describe('isPayload()', () => {
+    /** @type Http */
+    let http;
+  
+    beforeEach(() => {
+      http = new Http();
+    });
+
+    it('Returns a boolean', () => {
+      const result = http.payload.isPayload();
+      assert.typeOf(result, 'boolean');
+    });
+
+    it('Always returns false for noPayload', () => {
+      const result = http.payload.isPayload({
+        noPayload: true
+      });
+      assert.isFalse(result);
+    });
+
+    it('Always returns true for forcePayload', () => {
+      const result = http.payload.isPayload({
+        force: true
+      });
+      assert.isTrue(result);
+    });
+  });
+
+  describe('urlEncoded()', () => {
+    /** @type Http */
+    let http;
+  
+    beforeEach(() => {
+      http = new Http();
+    });
+
+    it('returns a string', () => {
+      const result = http.payload.urlEncoded();
+      assert.typeOf(result, 'string');
+    });
+
+    it('has at least one value', () => {
+      const result = http.payload.urlEncoded();
+      assert.notEqual(result.indexOf('='), -1);
+    });
+  });
+
+  describe('json()', () => {
+    /** @type Http */
+    let http;
+  
+    beforeEach(() => {
+      http = new Http();
+    });
+
+    it('Returns a string', () => {
+      const result = http.payload.json();
+      assert.typeOf(result, 'string');
+    });
+
+    it('Is valid JSON', () => {
+      const result = http.payload.json();
+      const data = JSON.parse(result);
+      assert.typeOf(data, 'object');
+    });
+  });
+
+  describe('xml()', () => {
+    /** @type Http */
+    let http;
+  
+    beforeEach(() => {
+      http = new Http();
+    });
+
+    it('Returns a string', () => {
+      const result = http.payload.xml();
+      assert.typeOf(result, 'string');
+    });
+  });
+
+  describe('payload()', () => {
+    /** @type Http */
+    let http;
+  
+    beforeEach(() => {
+      http = new Http();
+    });
+
+    it('returns a string when no content type', () => {
+      const result = http.payload.payload();
+      assert.typeOf(result, 'string');
+    });
+
+    it('generates JSON data', () => {
+      const spy = sinon.spy(http.payload, 'json');
+      http.payload.payload('application/json');
+      assert.isTrue(spy.called);
+    });
+
+    it('returns a string for application/json', () => {
+      const result = http.payload.payload('application/json');
+      assert.typeOf(result, 'string');
+    });
+
+    it('generates xml data', () => {
+      const spy = sinon.spy(http.payload, 'xml');
+      http.payload.payload('application/xml');
+      assert.isTrue(spy.called);
+    });
+
+    it('returns a string for application/xml', () => {
+      const result = http.payload.payload('application/xml');
+      assert.typeOf(result, 'string');
+    });
+
+    it('generates url encoded data', () => {
+      const spy = sinon.spy(http.payload, 'urlEncoded');
+      http.payload.payload('application/x-www-form-urlencoded');
+      assert.isTrue(spy.called);
+    });
+
+    it('returns a string for application/xml', () => {
+      const result = http.payload.payload('application/x-www-form-urlencoded');
+      assert.typeOf(result, 'string');
+    });
+
+    it('returns a string for other types', () => {
+      const result = http.payload.payload('text/plain');
+      assert.typeOf(result, 'string');
+    });
+
+    it('returns random string for other types type', () => {
+      const result = http.payload.payload('unknown');
+      assert.isAbove(result.length, 1);
     });
   });
 });
